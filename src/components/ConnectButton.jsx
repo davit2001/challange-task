744910bfe0dbf9c49f79a4433773c80db5b56ddc
davit_hakobyan <davith@partao.com>
2025-09-26 09:40:01 +0400

    Fix connect button styling and disconnect button

--- src/components/ConnectButton.jsx
+++ src/components/ConnectButton.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { useWeb3 } from "../context/Web3Context";
-import { useWeb3ModalAccount, useWeb3Modal } from "web3modal-web3js/react";
+import { useWeb3ModalAccount, useWeb3Modal, useDisconnect } from "web3modal-web3js/react";
 import {FaWallet} from "react-icons/fa";
 import {toast} from "react-toastify";
 
 const ConnectButton = () => {
     const { address } = useWeb3();
-    const { open, disconnect } = useWeb3Modal();
+    const { open } = useWeb3Modal();
     const { isConnected } = useWeb3ModalAccount();
+    const { disconnect } = useDisconnect();
 
     const onConnect = async () => {
         try {
@@ -19,6 +20,7 @@ const ConnectButton = () => {
 
     const onDisconnect = async () => {
         try {
+            console.log('onDisconnect')
             await disconnect();
         } catch (err) {
             toast.error("Failed to disconnect wallet");
@@ -30,9 +32,9 @@ const ConnectButton = () => {
     }
 
     return (
-        <button onClick={onConnect} className="btn w-full mb-4 flex items-center justify-center">
+        <button onClick={onConnect} className="btn flex items-center justify-center">
             <FaWallet className="mr-2" />
-            Connect Wallet to Invest
+            Connect Wallet
         </button>
     )
 };
